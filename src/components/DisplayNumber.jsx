b102c01fa411f70d9ea4c277a17d77ad064d3c26
nwh20 <hnamwon@gmail.com>
2020-11-06 13:26:16 +0000

    class 2

--- src/components/DisplayNumber.jsx
+++ src/components/DisplayNumber.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import store from '../store';
 
 class DisplayNumber extends Component {
-    render() {
-      return (
-        <div>
-          <h1>Display Number</h1>
-          <input type="text" value={this.props.number} readOnly></input>
-        </div>
-      );
-    }
+  state = {
+    number: store.getState().number
   }
+
+  constructor(props) {
+    super(props);
+    store.subscribe(function() {
+      this.setState({number: store.getState().number});
+    }.bind(this))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Display Number</h1>
+        <input type="text" value={this.state.number} readOnly></input>
+      </div>
+    );
+  }
+}
 
 export default DisplayNumber;
