fbf9012dc479db186a9c05fd3a6438e618be6a3e
nwh20 <hnamwon@gmail.com>
2020-11-09 14:05:39 +0000

    class 3

---
+++ src/containers/DisplayNumber.jsx
@@ -0,0 +1,21 @@
+import DisplayNumber from '../components/DisplayNumber';
+import React, { Component } from 'react';
+import store from '../store';
+
+export default class DNC extends Component {
+  state = {
+    number: store.getState().number
+  }
+
+  constructor(props) {
+    super(props);
+
+    store.subscribe(function() {
+      this.setState({number: store.getState().number});
+    }.bind(this))
+  }
+
+  render() {
+    return <DisplayNumber number={this.state.number}></DisplayNumber>
+  }
+}
